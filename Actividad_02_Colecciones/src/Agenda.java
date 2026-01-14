0ebf6fe86128f284cf56bc55cd07c35d06f120d3
COMPU-S031\daw1 <kevin16032006@gmail.com>
2026-01-14 12:03:47 +0100

    no message

--- Actividad_02_Colecciones/src/Agenda.java
+++ Actividad_02_Colecciones/src/Agenda.java
@@ -1,4 +1,57 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Agenda {
+
+    private ArrayList<Contacto> contactos;
+
+    public Agenda() {
+        contactos = new ArrayList<>();
+    }
+
+    public void anadeContacto(Contacto c) {
+        for (Contacto contacto : contactos) {
+            if (contacto.getNombre().equalsIgnoreCase(c.getNombre())) {
+                System.out.println("El contacto ya existe.");
+                return;
+            }
+        }
+        contactos.add(c);
+        System.out.println("Contacto añadido correctamente.");
+    }
+
+    public void buscaContacto(String nombre) {
+        for (Contacto c : contactos) {
+            if (c.getNombre().equalsIgnoreCase(nombre)) {
+                System.out.println(c);
+                return;
+            }
+        }
+        System.out.println("Contacto no encontrado.");
+    }
+
+    public void eliminaContacto(String nombre) {
+        for (Contacto c : contactos) {
+            if (c.getNombre().equalsIgnoreCase(nombre)) {
+                contactos.remove(c);
+                System.out.println("Contacto eliminado.");
+                return;
+            }
+        }
+        System.out.println("Contacto no encontrado.");
+    }
+
+    public void visualizaAgenda() {
+        if (contactos.isEmpty()) {
+            System.out.println("La agenda está vacía.");
+        } else {
+            for (Contacto c : contactos) {
+                System.out.println(c);
+            }
+        }
+    }
+
+    public int numeroContactos() {
+        return contactos.size();
+    }
 }
+
--- Actividad_02_Colecciones/src/Contacto.java
+++ Actividad_02_Colecciones/src/Contacto.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
-
 public class Contacto {
+
+    private String nombre;
+    private String telefono;
+    private String correo;
+
+    public Contacto(String nombre, String telefono, String correo) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+
+    public static boolean validarNombre(String nombre) {
+        return nombre.matches("[A-Z][a-zA-Z]*");
+    }
+
+    public static boolean validarTelefono(String telefono) {
+        return telefono.matches("[679]\\d{8}");
+    }
+
+    public static boolean validarCorreo(String correo) {
+        return correo.matches("[a-zA-Z0-9_.-]+@[a-z]+\\.[a-z]{2,4}");
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre + ", Teléfono: " + telefono + ", Correo: " + correo;
+    }
 }
+
